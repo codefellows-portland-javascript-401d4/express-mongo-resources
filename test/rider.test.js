6b4f07931eadcfecbdc64bab05121226955e6cee
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-02 14:26:04 -0700

    Initial commit; Rider model and Rider unit tests passing

---
+++ test/rider.test.js
@@ -0,0 +1,44 @@
+const Rider = require('../lib/models/rider');
+const expect = require('chai').expect;
+
+describe ('Rider model', () => {
+
+  it ('validates with name and team', (done) => {
+    const test_rider = new Rider({
+      name: 'Marco Velo',
+      team: 'Francaise de Jeux'
+    });
+
+    test_rider.validate((err) => {
+      done(err);
+    });
+  });
+
+  it ('name is required', (done) => {
+    const test_rider = new Rider({
+      team: 'Astana'
+    });
+
+    test_rider.validate((err) => {
+      if (!err) done('rider name should have been required');
+      expect(err).to.be.ok;
+      done();
+    });
+  });
+
+  it ('role defaults to domestique', (done) => {
+    const test_rider = new Rider({
+      name: 'Alexander Vinokourov',
+      team: 'Astana'
+    });
+
+    test_rider.validate((err) => {
+      if (err) done(err);
+      else {
+        expect(test_rider.role).to.be.equal('domestique');
+        done();
+      }
+    });
+  });
+
+});
---
+++ test/riders.api.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe ('', () => {
+
+  before(() => {
+
+  });
+
+  it ('', () => {
+
+  });
+
+  after(() => {
+
+  });
+
+});
