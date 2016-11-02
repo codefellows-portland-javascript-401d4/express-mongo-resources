9d7d98689bfb73640b0dac6216032f8dfd1c3ffe
Tyler Smith <tyler.smith6891@gmail.com>
2016-11-02 15:54:46 -0700

    second resource added

---
+++ year-sorter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var arr = ['1975', '2001', '1986', '1944', '1964'];
+
+var numArr = arr.map(function(string) {
+    return Number(string);
+});
+
+numArr.forEach(i => {
+    
+});
+
