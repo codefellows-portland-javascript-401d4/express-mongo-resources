e88579f5fb649fbab59046dd76ecad584470cab8
Albert Reel <waxhoya@gmail.com>
2016-11-02 14:47:15 -0700

    First test complete

---
+++ test/gamechar.test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Gamechar = require('../lib/models/gamechar');
+
+
+describe('Checks that app properly routes for gamechars', () =>{
+
+    it('Validates name, game, age, and attack Power', done => {
+        const daffyDuck = new Gamechar({
+            name: 'Daffy Duck',
+            game: 'SpaceJam',
+            age: 79,
+            attackpower: 4
+        })
+
+        daffyDuck.validate(err => {
+            if (err) done(err);
+            else done();
+        })
+    })
+
+})
