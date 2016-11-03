7982a21567e22c42895f161b3c13ae5de3caa586
Gloria Guy <totallygloria@gmail.com>
2016-11-02 19:16:30 -0700

    I had forgotten to include the mongoose setup in the server, so it wasn't connecting to the database anywhere. Adding that fixed the 'not getting any data back' but in the GET method. Duh.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const http = require('http');
 const app = require('./lib/app');
 const port = process.env.PORT || 3000;
+require('./lib/setup-mongoose');
 
 const server = http.createServer(app);
 
