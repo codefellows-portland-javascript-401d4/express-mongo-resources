3cbf04fd1c8ed26bbc08d16aded21e97b0b72756
Gloria Guy <totallygloria@gmail.com>
2016-11-03 14:04:33 -0700

    First test of gitting the get all route works. Important note: superAgent returns it's own response object, if you want the text of that, be sure to use res.text (just like you'd use res.code for the status code)

---
+++ test/beers.test.js
@@ -0,0 +1,30 @@
+/** Created by Gloria Anholt on 11/3/16. **/
+
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+require( '../lib/setup-mongoose' );
+const app = require('../lib/app');
+
+describe('the beer routes and models', () => {
+
+  const server = chai.request(app);
+
+  it('requests beer api and gets results from the db', done => {
+
+    const expectedResults = '[{"_id":"581a8d26ca8a26db175663f7","name":"Belmont Station","streetAddress":"4500 SE Stark St","zipcode":97215,"phone":5032328538,"brewery":false,"bestBeer":null,"visited":true},{"_id":"581a8d34ca8a26db175663f8","name":"Lompoc 5th Quadrant","streetAddress":"3901 N Williams Ave","zipcode":97227,"phone":5032883996,"brewery":true,"bestBeer":"Proletariat Red","visited":true},{"_id":"581a8d3dca8a26db175663f9","name":"Laurelwood Public House & Brewery","streetAddress":"5115 NE Sandy Blvd","zipcode":97213,"phone":5032820622,"brewery":true,"bestBeer":"Workhorse IPA","visited":true}]';
+
+    server
+      .get('/api/beer')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, expectedResults);
+        done();
+      });
+
+  });
+
+
+});
