65085bee9ed656c957ee88d656b0e080955b2660
David Chase <dchasepdx@gmail.com>
2016-11-03 15:45:14 -0700

    unit testing finished

--- test/unit-testing.js
+++ test/unit-testing.js
@@ -1,8 +1,9 @@
 const Team = require('../lib/models/team');
-const Region = require('../lib/models/team');
+const Region = require('../lib/models/region');
 const assert = require('chai').assert;
 
 describe('Team model', () => {
+
   it('should validate with teamName, teamMembers, region, and tiWinner', done => {
     const team =  new Team ({
       teamName: 'team',
@@ -10,6 +11,7 @@ describe('Team model', () => {
       region: 'NA',
       tiWinner: true
     });
+
     team.validate(err => {
       if (!err) done();
       else done(err);
@@ -21,6 +23,7 @@ describe('Team model', () => {
       teamMembers: [1,2,3,4,5],
       tiWinner: false
     });
+
     team.validate(err => {
       assert.isOk(err, 'it should error');
       done();
@@ -34,14 +37,49 @@ describe('Team model', () => {
       region: 'NA',
       tiWinner: true
     });
+
     team.validate(err => {
       assert.isArray(team.teamMembers);
       done();
     });
   });
+});
 
-  it('should')
-  
+describe('Region model', () => {
 
+  it('should validate with region and teams', done => {
+    const region = new Region ({
+      region: 'SEA',
+      teams: ['team 1, team 2, team 3']
+    });
+
+    region.validate(err => {
+      assert.isOk(!err, 'do not expect an erro');
+      done();
+    });
+  });
+
+  it('should require a region', done => {
+    const region = new Region ({
+      teams: ['team 1, team 2, team 3']
+    });
+
+    region.validate(err => {
+      assert.isOk(err, 'expect an error here');
+      done();
+    });
+  });
+
+  it('should coerce teams to an array', done => {
+    const region = new Region ({
+      region: 'NA',
+      teams: 'team 1, team 2, team 3'
+    });
+
+    region.validate(err => {
+      assert.isArray(region.teams, 'expect teams to be an array');
+      done();
+    });
+  });
 
 });
