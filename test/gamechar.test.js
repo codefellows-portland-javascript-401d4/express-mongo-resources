9bb20c300b1501b423617a4715613017511746a0
Christopher Bruner <chrbruner9@gmail.com>
2016-11-02 21:59:29 -0700

    have unit testing running for gamemap testing

--- test/gamechar.test.js
+++ test/gamechar.test.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const assert = chai.assert;
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
 const Gamechar = require('../lib/models/gamechar');
-
 
 describe('Checks that app properly routes for gamechars', () =>{
 
@@ -11,12 +12,12 @@ describe('Checks that app properly routes for gamechars', () =>{
             game: 'SpaceJam',
             age: 79,
             attackpower: 4
-        })
+        });
 
         daffyDuck.validate(err => {
             if (err) done(err);
             else done();
-        })    
+        });    
     });
     
     it('Validates that the name is required', done =>{
