40ffc750ab902ccd6c7f1aa34d99c6d607f8c4c8
Gloria Guy <totallygloria@gmail.com>
2016-11-04 14:21:32 -0700

    Refactored tests to not rely on my own local database instance. Needed to take out the hardcoded expects -- I won't know what the new _id's are, so I'm just checking that the values that should have been set were set. New tests pass.

--- test/beers.test.js
+++ test/beers.test.js
@@ -27,40 +27,38 @@ describe('the beer routes and models', () => {
     }
   });
 
-  it('requests beer api and gets results from the db', done => {
-
-    const expectedResults = '[{"_id":"581a8d26ca8a26db175663f7","name":"Belmont Station","streetAddress":"4500 SE Stark St","zipcode":97215,"phone":5032328538,"brewery":false,"bestBeer":null,"visited":true},{"_id":"581a8d34ca8a26db175663f8","name":"Lompoc 5th Quadrant","streetAddress":"3901 N Williams Ave","zipcode":97227,"phone":5032883996,"brewery":true,"bestBeer":"Proletariat Red","visited":true},{"_id":"581a8d3dca8a26db175663f9","name":"Laurelwood Public House & Brewery","streetAddress":"5115 NE Sandy Blvd","zipcode":97213,"phone":5032820622,"brewery":true,"bestBeer":"Workhorse IPA","visited":true}]';
-
-    server
-      .get('/api/beer')
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.equal(res.text, expectedResults);
-        done();
-      });
-
-  });
 
 
-  it('queries by a brewery name and gets results', done => {
+  it('posts new breweries to the list', done => {
 
-    const expectedResults = '{"_id":"581a8d26ca8a26db175663f7","name":"Belmont Station","streetAddress":"4500 SE Stark St","zipcode":97215,"phone":5032328538,"brewery":false,"bestBeer":null,"visited":true}';
-
-    server
-      .get('/api/beer/Belmont%20Station') // there's a space in the name
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.equal(res.text, expectedResults);
-        done();
-      });
-
-  });
-
-  it('posts a new brewery to the list, visible in a get all', done => {
-
-    const expectedResults = '[{"_id":"581a8d26ca8a26db175663f7","name":"Belmont Station","streetAddress":"4500 SE Stark St","zipcode":97215,"phone":5032328538,"brewery":false,"bestBeer":null,"visited":true},{"_id":"581a8d34ca8a26db175663f8","name":"Lompoc 5th Quadrant","streetAddress":"3901 N Williams Ave","zipcode":97227,"phone":5032883996,"brewery":true,"bestBeer":"Proletariat Red","visited":true},{"_id":"581a8d3dca8a26db175663f9","name":"Laurelwood Public House & Brewery","streetAddress":"5115 NE Sandy Blvd","zipcode":97213,"phone":5032820622,"brewery":true,"bestBeer":"Workhorse IPA","visited":true}, {"name":"Deschutes","phone":5038675309,"brewery":true,"visited":true}]';
-
+    const Belmont =  {"name":"Belmont Station","streetAddress":"4500 SE Stark St","zipcode":97215,"phone":5032328538,"brewery":false,"bestBeer":null,"visited":true};
+    const Lompoc = {"name":"Lompoc 5th Quadrant","streetAddress":"3901 N Williams Ave","zipcode":97227,"phone":5032883996,"brewery":true,"bestBeer":"Proletariat Red","visited":true};
+    const Laurelwood = {"name":"Laurelwood Public House & Brewery","streetAddress":"5115 NE Sandy Blvd","zipcode":97213,"phone":5032820622,"brewery":true,"bestBeer":"Workhorse IPA","visited":true};
     const Deschutes = {"name":"Deschutes","phone":5038675309,"brewery":true,"visited":true};
+
+    server
+      .post('/api/beer')
+      .send(Belmont)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, 'Belmont Station added!');
+      });
+
+    server
+      .post('/api/beer')
+      .send(Lompoc)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, 'Lompoc 5th Quadrant added!');
+      });
+
+    server
+      .post('/api/beer')
+      .send(Laurelwood)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, 'Laurelwood Public House & Brewery added!');
+      });
 
     server
       .post('/api/beer')
@@ -71,11 +69,33 @@ describe('the beer routes and models', () => {
         done();
       });
 
+  });
+
+  // local db testing only
+  it('queries by a brewery name and gets results', done => {
+
+    const expectedResults = {
+      "name":"Belmont Station",
+      "streetAddress":"4500 SE Stark St",
+      "zipcode":97215,
+      "phone":5032328538,
+      "brewery":false,
+      "bestBeer":null,
+      "visited":true
+    };
+
     server
-      .get('/api/beer')
+      .get('/api/beer/Belmont%20Station') // there's a space in the name
       .end((err, res) => {
         if (err) return done(err);
-        assert.equal(res.text, expectedResults);
+        let results = JSON.parse(res.text);
+        assert.equal(results.name, expectedResults.name);
+        assert.equal(results.streetAddress, expectedResults.streetAddress);
+        assert.equal(results.zipcode, expectedResults.zipcode);
+        assert.equal(results.phone, expectedResults.phone);
+        assert.equal(results.brewery, expectedResults.brewery);
+        assert.equal(results.bestBeer, expectedResults.bestBeer);
+        assert.equal(results.visited, expectedResults.visited);
         done();
       });
 
@@ -107,8 +127,6 @@ describe('the beer routes and models', () => {
 
   it('deletes a record by name', done => {
 
-    const expectedResults = '[{"_id":"581a8d26ca8a26db175663f7","name":"Belmont Station","streetAddress":"4500 SE Stark St","zipcode":97215,"phone":5032328538,"brewery":false,"bestBeer":null,"visited":true},{"_id":"581a8d34ca8a26db175663f8","name":"Lompoc 5th Quadrant","streetAddress":"3901 N Williams Ave","zipcode":97227,"phone":5032883996,"brewery":true,"bestBeer":"Proletariat Red","visited":true},{"_id":"581a8d3dca8a26db175663f9","name":"Laurelwood Public House & Brewery","streetAddress":"5115 NE Sandy Blvd","zipcode":97213,"phone":5032820622,"brewery":true,"bestBeer":"Workhorse IPA","visited":true}]';
-
     server
       .del('/api/beer/Deschutes')
       .end((err, res) => {
@@ -117,11 +135,10 @@ describe('the beer routes and models', () => {
       });
 
     server
-      .get('/api/beer')
+      .get('/api/beer/Deschutes')
       .end((err, res) => {
-        if (err) return done(err);
-        assert.equal(res.text, expectedResults);
-        done();
+        if (err) done();
+        else done(err);
       });
 
   });
