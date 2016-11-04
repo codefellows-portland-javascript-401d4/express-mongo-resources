5294672333732e5da79e43f50688eb29178bba14
Michael Freeman <mjfreem1@gmail.com>
2016-11-03 19:10:11 -0700

    nearly finished with validation

---
+++ test/country.test.js
@@ -0,0 +1,54 @@
+const Country = require('../lib/models/city');
+const assert = require('chai').assert;
+
+describe('Country model', () => {
+
+    it('validates with name, religion, and degree of American', done => {
+        const country = new Country({
+            name: 'Blahblahkia',
+            religions: ['Mammon'],
+            freedom: true
+        });
+
+        country.validate(err => {
+            if(!err) done();
+            else done(err);
+        });
+    });
+
+    it('requires name', done => {
+        const country = new Country({
+            religions: ['Satanism'],
+            freedom: true
+        });
+
+        country.validate(err => {
+            assert.isOk(err, 'name not present');
+            done();
+        });
+    });
+
+    it('religions is an array', done => {
+        const country = new Country({
+            name: 'Stanistan',
+            religions: 5
+        });
+
+        country.path('religions').validate(err => {
+            assert.isOk(err, 'religions is not an array');
+            done();
+        });
+    });
+
+    it('requires freedom to be a Boolean', done => {
+        const country = new Country({
+            name: 'Hebetude',
+            freedom: 'no'
+        });
+
+        country.validate(err => {
+            assert.isOk(err, 'freedom is neither a choice or a Boolean');
+            done();
+        });
+    });
+});
