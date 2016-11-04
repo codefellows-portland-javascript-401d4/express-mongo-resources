0265c4c379faa31c589b67909210439626c576cf
Tyler Smith <tyler.smith6891@gmail.com>
2016-11-03 17:38:46 -0700

    routes and tests done

--- year-sorter.js
+++ year-sorter.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var arr = ['1975', '2001', '1986', '1944', '1964'];
+const arr = ['500', '1986', '1944', '1964', '826'];
+const orderedArr = [];
 
 var numArr = arr.map(function(string) {
     return Number(string);
 });
 
-numArr.forEach(i => {
-    
+numArr.sort(function(a, b) {
+    return b - a;
 });
 
+console.log(numArr);
